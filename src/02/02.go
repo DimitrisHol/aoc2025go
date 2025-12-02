cf42037a6f8cdd205b17cc3df0e480ef5c9e5135
Dimitris Hol <dimitrishol@protonmail.com>
2025-12-03 00:26:13 +0200

    Part 2 ⭐

--- src/02/02.go
+++ src/02/02.go
@@ -5,23 +5,23 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
-
-	// "strconv"
 	"strings"
 )
 
 func parseFile(filename string) []string {
 	currentPath, _ := os.Getwd()
-	testPath := filepath.Join(currentPath, "..", "..", "input", "test", filename)
-	// testPath := filepath.Join(currentPath, "..", "..", "input", filename)
+	// testPath := filepath.Join(currentPath, "..", "..", "input", "test", filename)
+	testPath := filepath.Join(currentPath, "..", "..", "input", filename)
 
 	data, _ := os.ReadFile(testPath)
 	return strings.Split(string(data), ",")
 }
 
-func part1(ranges []string) int {
+func part1(ranges []string) (int, int) {
 
 	totalSumPart1 := 0
+	totalSumPart2 := 0
+
 	for _, v := range ranges {
 
 		standEndRange := strings.Split(v, "-")
@@ -34,10 +34,14 @@ func part1(ranges []string) int {
 			if invalidIdPart1(strconv.Itoa(i)) {
 				totalSumPart1 += i
 			}
+
+			if invalidIdPart2(strconv.Itoa(i)) {
+				totalSumPart2 += i
+			}
 		}
 	}
 
-	return totalSumPart1
+	return totalSumPart1, totalSumPart2
 }
 
 func invalidIdPart1(id string) bool {
@@ -49,6 +53,52 @@ func invalidIdPart1(id string) bool {
 
 	return firstPart == secondPart
 
+}
+
+func invalidIdPart2(id string) bool {
+
+	// [0, 3] = [121 121]    , next [3:6]
+	// [0, 2] = [12 12 12]   , next [2:4], [4:6]
+	// [0, 1] = [1 2 1 2 1 2], next [1:2], [2:3], [3:4], [4:5], [5:6]
+
+	length := len(id)
+
+	// Search for the first split in x parts that makes it invalid
+	for parts := 2; parts <= 10; parts++ {
+
+		// Exit early if you can't divide the id into equal blocks
+		if length%parts != 0 || length < parts {
+			continue
+		}
+
+		// println("Id : ", id, "trying to split into ", parts, "parts.")
+
+		blockSize := length / parts
+		firstBlock := id[0:blockSize]
+
+		allBlocksEqual := true
+
+		// Check if all other blocks are equal to the first block
+		for i := 1; i < parts; i++ { // loop for parts -1 times (the rest of the blocks)
+
+			start := i * blockSize
+			stop := (i * blockSize) + blockSize
+
+			nextBlock := id[start:stop]
+
+			// println("checking first block equals with ", i+1, "block :", firstBlock, "==", nextBlock)
+			if firstBlock != nextBlock {
+				allBlocksEqual = false
+				break
+			}
+		}
+
+		if allBlocksEqual {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() {
