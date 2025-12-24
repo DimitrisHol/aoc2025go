ce81e6cf9f29d83b7335a3ef6eeeae181ba41171
Dimitris Hol <dimitrishol@protonmail.com>
2025-12-24 16:38:17 +0200

    Day 07 Part 1 t 02 ⭐⭐

--- src/07/07.go
+++ src/07/07.go
@@ -91,8 +91,81 @@ func part1(data []string) int {
 	return len(visitedNodes)
 }
 
+func part2(data []string) int {
+
+	numberOfRows := len(data)
+	numberOfCols := len(data[0])
+
+	// Convert the []string into [][]rune to access
+	grid := make([][]rune, numberOfRows)
+	for i := 0; i < numberOfRows; i++ {
+
+		grid[i] = make([]rune, numberOfCols) // Create the new slice
+		row := []rune(data[i])               // Convert row -> []rune
+
+		for j := 0; j < numberOfCols; j++ {
+			grid[i][j] = row[j]
+		}
+	}
+
+	// Step 1 : Find the S position
+	startY := 0
+	for i := 0; i < numberOfCols; i++ {
+		if grid[0][i] == 'S' {
+			startY = i
+			break
+		}
+	}
+
+	cache := map[tile]int{}
+
+	var traverse func(tile) int
+	traverse = func(startTile tile) int {
+
+		if (startTile.x) == numberOfRows-1 {
+			return 1
+		}
+
+		nextTile := tile{x: startTile.x + 1, y: startTile.y}
+
+		// Normal tile
+		switch grid[nextTile.x][nextTile.y] {
+		case '.':
+			return traverse(nextTile)
+
+		case '^':
+
+			leftTile := tile{x: nextTile.x, y: nextTile.y - 1}
+			leftTileCount := 0
+
+			if count, exists := cache[leftTile]; exists {
+				leftTileCount = count
+			} else {
+				leftTileCount = traverse(leftTile)
+				cache[leftTile] = leftTileCount
+			}
+
+			rightTile := tile{x: nextTile.x, y: nextTile.y + 1}
+			rightTileCount := 0
+
+			if count, exists := cache[rightTile]; exists {
+				rightTileCount = count
+			} else {
+				rightTileCount = traverse(rightTile)
+				cache[rightTile] = rightTileCount
+			}
+
+			return leftTileCount + rightTileCount
+		}
+		return 0
+	}
+
+	return traverse(tile{x: 0, y: startY})
+}
+
 func main() {
 
 	data := parseFile("07.txt")
 	fmt.Println(part1(data))
+	fmt.Println(part2(data))
 }
